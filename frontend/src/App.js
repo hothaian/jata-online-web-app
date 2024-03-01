116b7f3705dfe59b8704a4318a0cf793ec22bb86
An Ho <41894751+hothaian@users.noreply.github.com>
2024-03-01 15:25:53 -0800

    Committing local changes before branch switch

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -21,12 +21,8 @@ import SignUp from './components/SignUp';
 import { About } from './pages/About';
 import { Home } from './pages/Home';
 import Profile from './pages/Profile';
-<<<<<<< HEAD:src/App.js
-import { SingleSellPost } from './components/singleSellPost';
-=======
 import PieChart from './components/Charts/PieChart';
 import BarChart from './components/Charts/BarChart';
->>>>>>> origin/an-ho:frontend/src/App.js
 
 const App = () => {  
   return (
@@ -40,13 +36,8 @@ const App = () => {
           <Route path="/signup" element={<SignUp />} />
           <Route path="/profile" element={<Profile />} />
           <Route path="/cart" element={<ShoppingCart />} />
-<<<<<<< HEAD:src/App.js
-          <Route path="/sellpost/:sellPostID" element={<SingleSellPost />} />
-
-=======
           <Route path="/piechart" element={<PieChart />} />
           <Route path="/barchart" element={<BarChart/>} />
->>>>>>> origin/an-ho:frontend/src/App.js
         </Routes>
       </div>
     </Router>
