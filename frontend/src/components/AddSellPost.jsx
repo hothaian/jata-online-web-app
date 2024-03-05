371eee0f95dd14cd25d39476b63f61b8bcf2c9f4
An Ho <41894751+hothaian@users.noreply.github.com>
2024-03-04 17:03:08 -0800

    image after uploaded will save in mySQL local , and notify the uploaded sucess.

--- frontend/src/components/AddSellPost.jsx
+++ frontend/src/components/AddSellPost.jsx
@@ -1,46 +1,199 @@
-import { Button, Checkbox, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, FormControlLabel, IconButton, Stack, TextField } from "@mui/material";
-import FormControlContext from "@mui/material/FormControl/FormControlContext";
-import CloseIcon from "@mui/icons-material/Close"
-import { useState } from "react";
+import React, { useState } from "react";
+import {
+  Button,
+  Checkbox,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+  FormControlLabel,
+  IconButton,
+  Stack,
+  TextField,
+} from "@mui/material";
+import CloseIcon from "@mui/icons-material/Close";
 import ImageUploader from "./ImageHandle/ImageUploader";
-
+import axios from "axios";
 
 const AddSellPost = () => {
-    const [open,openchange]=useState(false);
-    const functionopenpopup=()=>{
-        openchange(true);
+  const [open, setOpen] = useState(false);
+  const [formData, setFormData] = useState({
+    seller_id: 2,
+    item_name: "",
+    description: "",
+    price: "",
+    quantity: 0,
+    size: "",
+    gender: "",
+    color: "",
+    brand: "",
+    picUrl: "pictureurl", // Update with actual image URL
+    condition: "",
+    categoryNames: ["Shoe"],
+  });
+
+  const handleOpenPopup = () => {
+    setOpen(true);
+  };
+
+  const handleClosePopup = () => {
+    setOpen(false);
+  };
+
+  const handleFormChange = (event) => {
+    const { name, value } = event.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleFormSubmit = async () => {
+    try {
+      console.log(formData);
+      const response = await axios.post(
+        "http://localhost:8080/api/sellposts",
+        formData,{        headers: {
+            'Content-Type': 'application/json',
+          },}
+      );
+
+      if (response.status === 200) {
+        // Handle successful response (e.g., show a success message)
+        console.log("Sell post submitted successfully!");
+        handleClosePopup();
+      } else {
+        // Handle error response (e.g., show an error message)
+        console.error("Failed to submit sell post:", response.statusText);
+      }
+      handleClosePopup();
+    } catch (error) {
+      console.error("Error submitting sell post:", error);
     }
-    const closepopup=()=>{
-        openchange(false);
-    }
-    return (
-        <div style={{textAlign:'center'}}>
-           
-            <Button onClick={functionopenpopup} color="primary" variant="contained">Add Sell Post </Button>
-            <Dialog 
-            // fullScreen 
-            open={open} onClose={closepopup} fullWidth maxWidth="sm">
-                <DialogTitle>Add Sell Post <IconButton onClick={closepopup} style={{float:'right'}}><CloseIcon color="primary"></CloseIcon></IconButton>  </DialogTitle>
-                <DialogContent>
-                    {/* <DialogContentText>Do you want remove this user?</DialogContentText> */}
-                    <Stack spacing={2} margin={2}>
-                      <TextField variant="outlined" label="Username"></TextField>
-                      <TextField variant="outlined" label="Password"></TextField>
-                      <TextField variant="outlined" label="Email"></TextField>
-                      <TextField variant="outlined" label="Phone"></TextField>
-                      <ImageUploader/>
-                
-                      <FormControlLabel control={<Checkbox defaultChecked color="primary"></Checkbox>} label="Agree terms & conditions"></FormControlLabel>
-                      <Button color="primary" variant="contained">Submit</Button>
-                    </Stack>
-                </DialogContent>
-                <DialogActions>
-                {/* <Button color="success" variant="contained">Yes</Button>
-                    <Button onClick={closepopup} color="error" variant="contained">Close</Button> */}
-                </DialogActions>
-            </Dialog>
-        </div>
-    );
-}
+  };
+
+  const handleCategoryChange = (event) => {
+    const { value } = event.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      categoryNames: value.split(",").map((category) => category.trim()),
+    }));
+  };
+
+
+  const handleUploadComplete = (downloadURL) => {
+    // Update the picUrl in formData with the downloaded image URL
+    setFormData((prevData) => ({
+      ...prevData,
+      picUrl: downloadURL,
+    }));
+  };
+
+  return (
+    <div style={{ textAlign: "center" }}>
+      <Button onClick={handleOpenPopup} color="primary" variant="contained">
+        Add Sell Post
+      </Button>
+      <Dialog open={open} onClose={handleClosePopup} fullWidth maxWidth="sm">
+        <DialogTitle>
+          New Sell Post
+          <IconButton onClick={handleClosePopup} style={{ float: "right" }}>
+            <CloseIcon color="primary"></CloseIcon>
+          </IconButton>
+        </DialogTitle>
+        <DialogContent>
+          <Stack spacing={2} margin={2}>
+            <TextField
+              variant="outlined"
+              label="Item Name"
+              name="item_name"
+              value={formData.item_name}
+              onChange={handleFormChange}
+            />
+            <TextField
+              variant="outlined"
+              label="Description"
+              name="description"
+              value={formData.description}
+              onChange={handleFormChange}
+            />
+            <TextField
+              variant="outlined"
+              label="Price"
+              name="price"
+              value={formData.price}
+              onChange={handleFormChange}
+            />
+            <TextField
+              variant="outlined"
+              label="Quantity"
+              name="quantity"
+              type="number"
+              value={formData.quantity}
+              onChange={handleFormChange}
+            />
+            <TextField
+              variant="outlined"
+              label="Size"
+              name="size"
+              value={formData.size}
+              onChange={handleFormChange}
+            />
+            <TextField
+              variant="outlined"
+              label="Gender"
+              name="gender"
+              value={formData.gender}
+              onChange={handleFormChange}
+            />
+            <TextField
+              variant="outlined"
+              label="Color"
+              name="color"
+              value={formData.color}
+              onChange={handleFormChange}
+            />
+            <TextField
+              variant="outlined"
+              label="Brand"
+              name="brand"
+              value={formData.brand}
+              onChange={handleFormChange}
+            />
+            <TextField
+              variant="outlined"
+              label="Condition"
+              name="condition"
+              value={formData.condition}
+              onChange={handleFormChange}
+            />
+            <TextField
+              variant="outlined"
+              label="Category Names (comma-separated)"
+              name="categoryNames"
+              value={formData.categoryNames.join(", ")}
+              onChange={handleCategoryChange}
+            />
+
+            <ImageUploader onUploadComplete={handleUploadComplete} />
+            <FormControlLabel
+              control={<Checkbox defaultChecked color="primary" />}
+              label="Agree terms & conditions"
+            />
+            <Button
+              color="primary"
+              variant="contained"
+              onClick={handleFormSubmit}
+            >
+              Submit
+            </Button>
+          </Stack>
+        </DialogContent>
+        <DialogActions>{/* Add additional actions if needed */}</DialogActions>
+      </Dialog>
+    </div>
+  );
+};
 
 export default AddSellPost;
--- frontend/src/components/ImageHandle/ImageUploader.js
+++ frontend/src/components/ImageHandle/ImageUploader.js
@@ -1,67 +1,86 @@
-import React, { useState, useEffect } from 'react';
-import { listAll, getDownloadURL, ref, uploadBytes } from 'firebase/storage';
-import { useAuth } from '../../context/AuthContext';
-import { storage } from '../../firebase/firebase';
+import React, { useState, useEffect } from "react";
+import { listAll, getDownloadURL, ref, uploadBytes } from "firebase/storage";
+import { useAuth } from "../../context/AuthContext";
+import { storage } from "../../firebase/firebase";
 
-const ImageUploader = () => {
+const ImageUploader = ({ onUploadComplete }) => {
   const { currentUser } = useAuth();
-  const [imageUrls, setImageUrls] = useState([]);
+  const [uploadedImageUrl, setUploadedImageUrl] = useState(null);
   const [selectedImage, setSelectedImage] = useState(null);
-  const [refreshKey, setRefreshKey] = useState(0); // State for triggering refresh
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [successMessage, setSuccessMessage] = useState(null);
 
   useEffect(() => {
-    const fetchImages = async () => {
-      try {
-        if (currentUser) {
-          const imagesRef = ref(storage, `images/${currentUser.uid}`);
-          const imageList = await listAll(imagesRef);
-
-          const urlsPromises = imageList.items.map(async (itemRef) => {
-            const url = await getDownloadURL(itemRef);
-            return url;
-          });
-
-          const urls = await Promise.all(urlsPromises);
-          setImageUrls(urls);
-        }
-      } catch (error) {
-        console.error('Error fetching images:', error);
-      }
-    };
-
-    fetchImages();
-  }, [currentUser, refreshKey]);
+    // Reset the uploaded image URL when currentUser changes
+    setUploadedImageUrl(null);
+  }, [currentUser]);
 
   const handleImageChange = (e) => {
     const file = e.target.files[0];
-    setSelectedImage(file);
+
+    // Add file type validation
+    if (file && file.type.startsWith("image/")) {
+      setSelectedImage(file);
+    } else {
+      setSelectedImage(null);
+      setError("Please select a valid image file.");
+      setSuccessMessage(null);
+    }
   };
 
   const handleUpload = async () => {
     try {
+      setError(null);
+      setLoading(true);
+
       if (selectedImage) {
-        const storageRef = ref(storage, `images/${currentUser.uid}/${selectedImage.name}`);
+        const storageRef = ref(
+          storage,
+          `images/${currentUser.uid}/${selectedImage.name}`
+        );
         await uploadBytes(storageRef, selectedImage);
+
+        // Get the download URL of the uploaded image
+        const downloadURL = await getDownloadURL(storageRef);
+
         setSelectedImage(null);
-        // Trigger a refresh of the ImageList component
-        setRefreshKey((prevKey) => prevKey + 1);
+
+        // Set the uploaded image URL to display
+        setUploadedImageUrl(downloadURL);
+
+        // Update the uploaded_url prop in the parent component
+        onUploadComplete(downloadURL);
+        // Show success message
+        setSuccessMessage("Image uploaded successfully!");
       }
     } catch (error) {
-      console.error('Error uploading image:', error);
+      console.error("Error uploading image:", error);
+      setError("Error uploading image. Please try again later.");
+    } finally {
+      setLoading(false);
     }
   };
 
   return (
     <div>
-      <p></p>
       <p>Add Image For Sell Post:</p>
       <div>
-        <input type="file" onChange={handleImageChange} />
-        <button onClick={handleUpload}>Upload Image</button>
+        <input type="file" accept="image/*" onChange={handleImageChange} />
+        <button onClick={handleUpload} disabled={!selectedImage || loading}>
+          Upload Image
+        </button>
       </div>
-      {imageUrls.map((url, index) => (
-        <img key={index} src={url} alt={`Image ${index}`} style={{ maxWidth: '300px', margin: '10px' }} />
-      ))}
+      {loading && <p>Loading...</p>}
+      {error && <p style={{ color: "red" }}>{error}</p>}
+      {successMessage && <p style={{ color: 'green' }}>{successMessage}</p>}
+      {uploadedImageUrl && (
+        <img
+          src={uploadedImageUrl}
+          alt="Uploaded Image"
+          style={{ maxWidth: "300px", margin: "10px" }}
+        />
+      )}
     </div>
   );
 };
