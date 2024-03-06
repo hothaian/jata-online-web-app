d1454ef8c9e1d266b191c926dc9184a3ea5613ee
phuhuutin <39242538+phuhuutin@users.noreply.github.com>
2024-03-06 13:14:15 -0800

    corrected API docs

--- Jata_Backend/routes/user.routes.js
+++ Jata_Backend/routes/user.routes.js
@@ -3,24 +3,28 @@ module.exports = app => {
     var router = require("express").Router();
   
     // Create a new Tutorial
-    // BODY:
     // {
-    //   "username": "phuhuutin",
-    //   "email": "john@example.com",
-    //   "role_id": 1,
-    //   "first_name": "Tin",
-    //   "last_name": "Doe",
-    //   "gender": "Male",
-    //   "date_of_birth": "1990-05-15T00:00:00.000Z",
-    //   "profile_pictureUrl": "https://img.freepik.com/free-photo/portrait-friendly-looking-happy-attractive-male-model-with-moustache-beard-wearing-trendy-transparent-glasses-smiling-broadly-while-listening-interesting-story-waiting-mom-give-meal_176420-22400.jpg",
-    //   "address": {
-    //           "street_address": "123 Main St",
-    //           "city": "Tacoma",
-    //           "state": "NY",
-    //           "zip_code": "10001",
-    //           "country": "USA"
+    //     "buyer_id": 1,
+    //     "total_price": "31.98",
+    //     "shipping_address": {
+    //         "user_id": 1,
+    //       "street_address": "123 Tacoma Tacoma TTacomaacoma St",
+    //       "city": "Tacoma",
+    //       "state": "NY",
+    //       "zip_code": "10001",
+    //       "country": "USA"
+    //     },
+    //     "items": [
+    //       {
+    //         "quantity": 2,
+    //         "sellpost_id": 2
+    //       },
+    //       {
+    //         "quantity": 1,
+    //         "sellpost_id": 1
     //       }
-    // }
+    //     ]
+    //   }
     router.post("/", user.create);
   
     // Retrieve all user
